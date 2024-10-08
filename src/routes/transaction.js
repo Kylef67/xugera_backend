48a38f025041cce276bf9a43a8410a60408b0a36
Manuel Lorenzo Pereira <lorenzo.pereira06@gmail.com>
2024-10-09 04:12:02 +0800

    Initial Commit

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -3,6 +3,9 @@ const transaction = require('../controllers/transaction');
 const router = express.Router();
 
 router.post('/transaction' , transaction.post);
-router.get('/transaction' , transaction.all)
+router.get('/transaction' , transaction.all);
+router.get('/transaction/:id' , transaction.get);
+router.put('/transaction/:id', transaction.update);
+router.delete('/transaction/:id', transaction.delete);
 
 module.exports = router
