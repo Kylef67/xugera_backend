d273ba4259dc75da1bcc4b9854af243d8f11a251
Manuel Lorenzo Pereira <lorenzo.pereira06@gmail.com>
2024-10-09 04:09:26 +0800

    Initial Commit

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -17,5 +17,31 @@ module.exports = {
         } catch (err) {
             res.status(500).json({ error: err.message })
         }
+    },
+    get: async (req, res) => {
+        try {
+            const transaction = await Transaction.findById(req.params.id);
+            res.json(transaction)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    update: async (req, res) => {
+        try {
+            const account = await Transaction.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            if (!account) return res.status(404).json({ error: 'Account not found' });
+            res.json(account);
+        } catch (err) {
+            res.status(400).json({ error: err.message });
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            const account = await Transaction.findByIdAndDelete(req.params.id);
+            if (!account) return res.status(404).json({ error: 'Account not found' });
+            res.json({ message: 'Account deleted' });
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
     }
 }
